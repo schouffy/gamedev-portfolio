1c1a208b2a1e04b275ac4a547ecd425d2782442f
Eduardo Barrancos <eduardobarrancos11488@gmail.com>
2023-08-19 16:34:10 +0100

    Trying to fix deployment

--- veu.config.js
+++ veu.config.js
@@ -1,6 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/gamedev-portfolio/'
-      : '/',
-    assetsPublicPath: ''
+    publicPath: '/gamedev-portfolio/'
 }
