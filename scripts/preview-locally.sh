eee54be89e25b601a5f269c689cee0119b55ed59
Eduardo Barrancos <eduardobarrancos11488@gmail.com>
2023-08-19 15:12:55 +0100

    Script to preview built project locally

---
+++ scripts/preview-locally.sh
@@ -0,0 +1,11 @@
+# Sourcing nvm
+. ~/.nvm/nvm.sh
+
+# build
+nvm install 16.20.2 
+npm run build
+
+npm install -g serve
+# -s flag means serve it in Single-Page Application mode
+# which deals with the routing problem below
+serve -s dist
--- veu.config.js
+++ veu.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
       ? '/gamedev-portfolio/'
-      : '/'
+      : '/',
+    assetsPublicPath: ''
 }
