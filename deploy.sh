32c888cc85d8f0078266d385788d4277f33b78db
Eduardo Barrancos <eduardobarrancos11488@gmail.com>
2023-08-19 14:56:49 +0100

    Improve deploy script - check branch executed in and install correct node version

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env sh
 
+if [ $(git branch --show-current) != 'main' ]
+then
+    echo "You need to be in the main branch to deploy"
+    exit 1
+fi
+
 # abort on errors
 set -e
 
+# Sourcing nvm
+. ~/.nvm/nvm.sh
+
 # build
+nvm install 16.20.2 
 npm run build
 
 # navigate into the build output directory
