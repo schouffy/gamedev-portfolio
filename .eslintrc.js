52cc6d5785e934a1d5f3927f8fb83c31b59fc0d1
Rachel Lowe <60523642+11BelowStudio@users.noreply.github.com>
2023-10-23 17:33:41 +0100

    disabling an eslint rule which is incorrect with vue3

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,8 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "vue/no-v-for-template-key" : "off" // disabling vue2 rule
   },
   settings: {
     "import/resolver": {
