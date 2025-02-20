1a133488247b573fbb646ada64ac6e9824fe3d66
Carlos-Ortega-Gonzalez <carlos.ortega.gonzalez@gmail.com>
2025-02-20 17:21:31 +0100

    WIP - User experience

--- src/views/VCA_UserExperience.vue
+++ src/views/VCA_UserExperience.vue
@@ -4,6 +4,58 @@
 
       <VCAMenu />
       
+      <div style="clear:both"></div>
+
+      <h3><strong>ENHANCED INPUT</strong></h3>
+      
+      <table>
+        <tr>
+          <td class="top">
+            <iframe class="youtube" src="https://www.youtube.com/embed/feuBT5AA8gw" frameborder="0" allowfullscreen></iframe>
+          </td>
+          <td class="top">
+            <div class="side-text">User input is setup using Unreal's <strong>Enhanced Input system</strong>. Each action the Player can take is defined as an <strong>Input Action</strong>, and further configured in an <strong>Input Mapping Context</strong>.</div>
+            
+            <br>
+
+            <div class="side-text">The Player can use both the <strong>Keyboard and Mouse</strong>, or a <strong>Gamepad</strong> to play in Training or Arena Matches.</div>
+            
+            <br>
+
+            <div class="side-text">The <strong>CommonUI plugin</strong> is used to display at runtime what the controls are in a <strong>Widget</strong>. If the Player swaps to another input device, the key icons get updated seamlessly.</div>
+          </td>
+        </tr>
+      </table>
+
+      <div><br></div>
+
+      <h3><strong>USER INTERFACE</strong></h3>
+                  
+      <table>
+        <tr>
+          <td class="top">
+            <iframe class="youtube" src="https://www.youtube.com/embed/feuBT5AA8gw" frameborder="0" allowfullscreen></iframe>
+          
+            <br>
+
+            <div class="paragraph-bottom">The project's <strong>DPI Scale Rule</strong> is set to <strong>Scale to Fit</strong> so that the UI scales correctly on different resolutions.</div>
+          </td>
+          <td class="top">
+            <div class="side-text"><strong>UMG Widgets</strong> comprise the game's UI, which is designed with a 16:9 ratio in mind. The <strong>Player Controller</strong> handles the HUD and other in-game menus, while the Main Menu and related sub-menus are controlled by the <strong>Main Menu level's BP</strong> directly.</div>
+            
+            <br>
+
+            <div class="side-text">The only tasks the Widgets carry out are displaying text, images and animations. Processing and fetching the data itself is <strong>decoupled</strong> from the UI.</div>
+            
+            <br>
+
+            <div class="side-text">Events are used to make UI updates <strong>efficient</strong>. For periodic operations, <strong>Timers</strong> that run less frequently than Tick() are used instead.</div>
+          </td>          
+        </tr>
+      </table>
+      
+      <div style="clear:both"></div>
+
       <VCAMenu />
     </div>
   </template>
@@ -26,9 +78,27 @@
     margin-bottom: 20px;
   }
   
+  .paragraph-bottom {
+    max-width: 700px;
+    margin-top: 40px;
+  }
+
   .photo {
     margin-top: 50px;
     text-align: center;
+  }
+
+  iframe.youtube {        
+        min-height: 360px;
+        min-width: 640px;
+  }
+
+  .side-text {
+    margin-left: 50px;
+  }
+
+  td.top {
+    vertical-align: top;
   }
   
   @media only screen and (min-width: 620px){
