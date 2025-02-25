b8953eedd69e43b8fe2dc8f611a107f1b904e7ae
Carlos-Ortega-Gonzalez <carlos.ortega.gonzalez@gmail.com>
2025-02-25 19:16:05 +0100

    WIP - Performance

--- src/views/VCA_Internals.vue
+++ src/views/VCA_Internals.vue
@@ -37,15 +37,15 @@
             <iframe class="youtube" src="https://www.youtube.com/embed/feuBT5AA8gw" frameborder="0" allowfullscreen></iframe>
           </td>
           <td class="top">
-            <div class="side-text">Every Sound and Music effect is managed by the Sound Subsystem. Sounds and Music Cues are given an identifier, and a Sound Data Asset links these ids to the actual Cues.</div>
+            <div class="side-text">Every Sound and Music effect is managed by the <strong>Sound Subsystem</strong>. Sounds and Music Cues are given an identifier, and a <strong>Sound Data Asset</strong> links these ids to the actual Cues.</div>
             
             <br>
 
-            <div class="side-text">This means that BPs, like the Vehicle Pawns or the Trains that drop loot, do not need to have references to the Cues themselves; instead all they need to know is the Id of the sounds they need to play, and ask the Sound Subsystem to play them.</div>
+            <div class="side-text">This means that BPs, like the Vehicle Pawns or the Trains that drop loot, do not need to have <strong>references to the Cues</strong> themselves; instead all they need to know is the Id of the sounds they need to play, and ask the Sound Subsystem to play them.</div>
             
             <br>
 
-            <div class="side-text">Different sounds have different requirements, like playing at a specific location (with some attenuation settings to avoid hearing distant sounds too loud) or playing attached to a moving Actor (like the Engine sound of Vehicles).</div>
+            <div class="side-text">Different sounds have different requirements, like playing at a specific <strong>location</strong> (with some attenuation settings to avoid hearing distant sounds too loud) or playing <strong>attached</strong> to a moving Actor (like the Engine sound of Vehicles).</div>
           </td>          
         </tr>
       </table>
@@ -60,15 +60,15 @@
             <iframe class="youtube" src="https://www.youtube.com/embed/feuBT5AA8gw" frameborder="0" allowfullscreen></iframe>
           </td>
           <td class="top">
-            <div class="side-text">Text1.</div>
+            <div class="side-text">The <strong>Game Options Persistance Subsystem</strong> handles loading and saving data to disk. 2 different <strong>USaveGame</strong> based classes are used, one to store Settings, and another to store Game progress (with up to 4 Game Slots).</div>
             
             <br>
 
-            <div class="side-text">Text2.</div>
+            <div class="side-text">A Widget displays a rotating <strong>Gear Icon</strong> when a load or save operation is ongoing, providing awareness to the Player.</div>
             
             <br>
 
-            <div class="side-text">Text3.</div>
+            <div class="side-text">Although the amount of data the game needs is small, all disk operations are performed <strong>asynchronously</strong> in order to avoid stuttering during Load and Save operations.</div>
           </td>
         </tr>
       </table>
@@ -81,17 +81,20 @@
         <tr>
           <td class="top">
             <iframe class="youtube" src="https://www.youtube.com/embed/feuBT5AA8gw" frameborder="0" allowfullscreen></iframe>
+            <br>
+
+            <div class="paragraph-bottom"><strong>Note:</strong> Some of the footage in this video was taken in <strong>PIE</strong> in order to display stats with a bigger size on screen. Performance is better when measured in a <strong>development build</strong>.</div>
           </td>
           <td class="top">
-            <div class="side-text">Text1.</div>
+            <div class="side-text">The game is rate limited by default to <strong>60 fps</strong> in order to reduce CPU usage. A number of <strong>profiling</strong> activities were performed during development, which highlighted that the bottleneck was the <strong>GPU</strong> rather than the <strong>CPU</strong>, so most of the focus has been put on reducing the GPU Time.</div>
             
             <br>
 
-            <div class="side-text">Text2.</div>
-            
+            <div class="side-text">The game uses <strong>Lumen</strong> for Global Illumination. Some tests have shown that Graphic cards below Epic's minimum requirements for Lumen do work well; however, the game suffers greatly with integrated cards.</div>
+
             <br>
 
-            <div class="side-text">Text3.</div>
+            <div class="side-text">Special attention has been paid to avoiding stutters due to <strong>Shader compilation</strong>.</div>
           </td>
         </tr>
       </table>
