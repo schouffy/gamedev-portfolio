2f1956727b99f0a12962aeab7c097c9fc28300ee
Carlos-Ortega-Gonzalez <carlos.ortega.gonzalez@gmail.com>
2025-03-02 17:40:03 +0100

    WIP - RA 2D Setup

--- src/views/RA_2DSetup.vue
+++ src/views/RA_2DSetup.vue
@@ -14,15 +14,15 @@
             <iframe class="youtube" src="https://www.youtube.com/embed/feuBT5AA8gw" frameborder="0" allowfullscreen></iframe>
           </td>
           <td class="top">
-            <div class="side-text">Text1.</div>
+            <div class="side-text"><strong>Unreal Engine</strong> always works in <strong>3D space</strong>, so 2D projects require some specific setup.</div>
             
             <br>
 
-            <div class="side-text">Text2.</div>
+            <div class="side-text">The main factor is using a Camera with an <strong>Ortographic view</strong> and positioning it in such a way that you can use Unreal's features like the <strong>Navigation mesh</strong> to allow movement for the AI controlled Enemies.</div>
             
             <br>
 
-            <div class="side-text">Text3.</div>
+            <div class="side-text">In this project I decided to not use <strong>Lumen</strong> or other Lighting features that Unreal provides, and instead I set the <strong>Viewport mode</strong> to <strong>Unlit</strong> so I could see every Texture based on their color, and nothing else.</div>
           </td>
         </tr>
       </table>
@@ -35,17 +35,21 @@
         <tr>
           <td class="top">
             <iframe class="youtube" src="https://www.youtube.com/embed/feuBT5AA8gw" frameborder="0" allowfullscreen></iframe>
+
+            <br>
+
+            <div class="paragraph-bottom">Enemy types are defined by an <strong>Enum</strong>, which is used later on in the spawning process to select the exact BP of the Enemy to be spawned.</div>
           </td>
           <td class="top">
-            <div class="side-text">Text1.</div>
+            <div class="side-text">Every spaceship is a <strong>Pawn</strong> that handles multiple animations via <strong>Flipbooks</strong>. Movement animations are swapped based on movement related variables, while others, like shooting projectiles, or exploding upon death, depend on <strong>Events</strong> or <strong>AI commands</strong>.</div>
             
             <br>
 
-            <div class="side-text">Text2.</div>
-
+            <div class="side-text">Enemies share the same functionality (although not every Enemy uses all of it), and change their looks based on a <strong>Data Asset</strong>.</div>
+           
             <br>
 
-            <div class="side-text">Text3.</div>
+            <div class="side-text">Asteroids and Bonus Carriers are simpler than Enemies and Bosses in terms of AI, but how they handle their visuals and collisions is very similar to any other Pawn.</div>
           </td>
         </tr>
       </table>
